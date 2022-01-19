c5d560a6ae271a1c9281a44c11e08b4c561a7e85
최다연 <dianarock@naver.com>
2022-01-19 13:02:07 +0900

    feat: FE-게시글 목록

---
+++ frontend/pages/board/data.js
@@ -0,0 +1,11 @@
+export default [
+    {
+        board_id : 0,
+        title : "공지사항입니다",
+        content : "이것은 공지사항입니다",
+        user_id : "ssafy",
+        tag : "notice",
+        likes : 1,
+        hit : 1,
+    }
+]
--- frontend/pages/board/index.js
+++ frontend/pages/board/index.js
@@ -1,9 +1,33 @@
 import Layout from "../../components/layout";
+import BoardList from "./BoardList";
+import styles from "../../styles/Board.module.css"
+import Link from "next/link";
 
 export default function Board() {
   return (
     <Layout>
-      <h1>Board</h1>
-    </Layout>
+      <div className={styles.main}>
+        <h1>Board</h1>
+        <div className={styles.menus}>
+          <Link href="/">
+              <a className={styles.link}>공지사항</a>
+          </Link>
+          <Link href="/">
+              <a className={styles.link}>자유게시판</a>
+          </Link>
+          <Link href="/">
+              <a className={styles.link}>질문게시판</a>
+          </Link>
+          <Link href="/">
+              <a className={styles.link}>정보공유</a>
+          </Link>
+          <Link href="/">
+              <a className={styles.link}>사람 구해요</a>
+          </Link>
+        </div>
+        <BoardList />
+      </div>
+  </Layout>
+    
   );
 }
