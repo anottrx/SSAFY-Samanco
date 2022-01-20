b9a9f1c11af480c6c6faa961f6b66a79adaa5017
uoayop <uoayop@kakao.com>
2022-01-20 16:49:59 +0900

    feat: 스터디 페이지 추가

--- frontend/pages/study/index.js
+++ frontend/pages/study/index.js
@@ -1,9 +1,49 @@
 import Layout from "../../components/layout";
+import Items from "../../components/Club/ItemList";
+import SearchBar from "../../components/Common/Search";
+import StackTagList from "../../components/Club/StackTagList";
+import styled from "@emotion/styled";
+import { Button } from "@mui/material";
+import Router from "next/router";
 
 export default function Study() {
+  const ItemWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: left;
+  `
+
+  const StudyActions = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  `
+
+  const CusButton = styled(Button)`
+    height: fit-content;
+  `
+  
   return (
     <Layout>
       <h1>Study</h1>
+      <ItemWrapper>
+        <StudyActions>
+          <SearchBar></SearchBar>
+          <CusButton variant="outlined" size="medium"
+            onClick={() => {
+              Router.push("/study/regist");
+            }}>
+            등록하기
+          </CusButton>
+        </StudyActions>
+
+        <StackTagList></StackTagList>
+        <Items></Items>
+      </ItemWrapper>
     </Layout>
   );
 }
