7f979f147009ea9c0755e81c10921c9d1be5ddda
최다연 <dianarock@naver.com>
2022-01-24 12:01:03 +0900

    fix:FE-게시글 상세보기 UI 수정

--- frontend/pages/board/BoardDetail.js
+++ frontend/pages/board/BoardDetail.js
@@ -1,13 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../components/layout";
-import { Paper, TextField, Box, Button, MenuItem} from "@mui/material";
+import { Paper, Box, Chip, Button, Grid, Stack, Divider, Typography} from "@mui/material";
 import styled from "@emotion/styled";
 import { useRouter } from 'next/router';
 import styles from "../../styles/Board.module.css"
+import Datas from "./data.js";
 
 export default function BoardDetail(props) {
     const router = useRouter();
-    const no = router.query.board_id;
+    const no = router.query.boardId;
+    let [articles,setArticles] = useState(Datas);
 
     const CusPaper = styled(Paper)`
         width: 100%;
@@ -29,22 +31,41 @@ export default function BoardDetail(props) {
     `
 
     useEffect(()=>{
-        console.log("아이디는"+no);
+        console.log(articles[no]);
     });
 
     return (
         <Layout>
             <h1>Board Detail</h1>
             <CusPaper>  
-                <TextField
-                    id="outlined-textarea"
-                    name="content"
-                    label="게시글 내용"
-                    placeholder="게시글 내용"
-                    fullWidth
-                    rows={30}
-                    multiline
-                />
+                <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                    <Box sx={{ my: 3, mx: 2 }}>
+                        <Grid container alignItems="center">
+                        <Grid item xs>
+                            <Typography gutterBottom variant="h2" component="div">
+                            {articles[no].title}
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography gutterBottom variant="h6" component="div">
+                            {articles[no].userId}
+                            </Typography>
+                        </Grid>
+                        </Grid>
+                        <Typography color="text.secondary" variant="body2">
+                        {articles[no].startDate}
+                        </Typography>
+                    </Box>
+                    <Divider variant="middle" />
+                    <Box sx={{ m: 2 }}>
+                        <Typography gutterBottom variant="body1">
+                        {articles[no].content}
+                        </Typography>
+                    </Box>
+                    <Box sx={{ mt: 3, ml: 1, mb: 1 }}>
+                        <Button>Add to cart</Button>
+                    </Box>
+                </Box>
             </CusPaper>
         </Layout>
     )
