b5c6e1dd54311f0aef9f49bccd8c1d3aa51bd85a
ryuna <anottrx@gmail.com>
2022-01-27 17:20:25 +0900

    feat: FE-페이지타이틀 추가

--- frontend/pages/myinfo/index.js
+++ frontend/pages/myinfo/index.js
@@ -3,8 +3,9 @@ import MyInfo from "../../components/User/MyInfo";
 import Router from "next/router";
 
 export default function MyInfoPage() {
-  
   useEffect(() => {
+    document.title = "내정보 | 싸피사만코";
+
     if (!sessionStorage.getItem("userId")) {
       alert("로그인하신 뒤에 사용가능합니다");
       Router.push("/login");
