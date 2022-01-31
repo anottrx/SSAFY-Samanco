c163d00ba4c1ee2d3b03e6710e9382916c470427
ryuna <anottrx@gmail.com>
2022-01-31 20:01:35 +0900

    style: FE-내정보 메뉴 마우스 효과 적용

--- frontend/pages/myinfo/myinfoMenu.js
+++ frontend/pages/myinfo/myinfoMenu.js
@@ -5,34 +5,52 @@ import { useRouter } from "next/router";
 
 const MyInfoMenu = () => {
   const { pathname } = useRouter(); // pathname: 현재 path
-  let [nickname, setNickname] = useState(null);
-
-  useEffect(() => {
-    setNickname(sessionStorage.getItem("nickname"));
-  }, [nickname]);
 
   let links = [
-    { path: "/", label: "메인" },
-    { path: "/myinfo", label: "내 정보" },
-    { path: "/myinfo/pw", label: "비밀번호 재설정" },
+    { path: "/", label: "메인", name: "/myinfo/main" },
+    { path: "/myinfo", label: "내 정보", name: "/myinfo" },
+    { path: "/myinfo/pw", label: "비밀번호 재설정", name: "/myinfo/pw" },
   ];
+
+  const onMouseOver = (e) => {
+    e.target.style.color = "black";
+  };
+
+  const onMouseOut = (e) => {
+    e.target.style.color = "#666666";
+  };
 
   return (
     <div className={styles.menus}>
       {links.map((link, index) => {
         return (
           <Link href={link.path} key={index}>
-            {pathname.split("/")[1] === link.path.split("/")[1] ? (
+            {pathname.split("/")[2] === link.name.split("/")[2] ? (
               <a
                 className={styles.link}
                 style={{
                   fontWeight: "bolder",
                 }}
               >
-                {link.label}
+                {link.label}|{pathname.split("/")[1]}|{link.path.split("/")[1]}
               </a>
             ) : (
-              <a className={styles.link}>{link.label}</a>
+              <a
+                className={styles.link}
+                style={{
+                  fontWeight: "bold",
+                  color: "#666666",
+                }}
+                onMouseEnter={(e) => {
+                  onMouseOver(e);
+                }}
+                onMouseOut={(e) => {
+                  onMouseOut(e);
+                }}
+              >
+                {link.label}|여기||{pathname.split("/")[2]}|
+                {link.path.split("/")[2]}
+              </a>
             )}
           </Link>
         );
