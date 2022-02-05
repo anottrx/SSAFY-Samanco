9950576a95cd6b1039319f2da3b3d080c8b1f7d9
ryuna <anottrx@gmail.com>
2022-02-05 16:43:51 +0900

    feat: FE-시간초 카운트다운

---
+++ frontend/components/Common/CountdownTimer.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import { Typography } from "@mui/material";
+
+export default function  CountdownTimer(props) {
+  const [seconds, setSeconds] = useState(3);
+  const [minutes, setMinutes] = useState(0);
+
+  useEffect(() => {
+    const countdown = setInterval(() => {
+      if (parseInt(seconds) > 0) {
+        setSeconds(parseInt(seconds) - 1);
+      }
+      if (parseInt(seconds) === 0) {
+        if (parseInt(minutes) === 0) {
+          clearInterval(countdown);
+          props.changeTimerHandle(false, "timer");
+        } else {
+          setMinutes(parseInt(minutes) - 1);
+          setSeconds(59);
+        }
+      }
+    }, 1000);
+    return () => clearInterval(countdown);
+  }, [minutes, seconds]);
+
+  return (
+    <Typography sx={{ fontSize: "12px", display: "inline" }}>
+      &nbsp;남은 시간&nbsp;{minutes}:
+      {Number(seconds) < 10 ? "0" + seconds : seconds}
+    </Typography>
+  );
+};
+
