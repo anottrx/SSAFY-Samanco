180f3fc127e7420f64cf6a58d24910f4737f2b29
uoayop <uoayop@kakao.com>
2022-02-06 17:35:47 +0900

    fix: FE-SWC 관련 패키지 삭제

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
-  swcMinify: true,
+  // swcMinify: true,
   // swcMinify : Rust 컴파일러로 변경 -> 속도 빨라짐
   // experimental: {
   //   // experimental : Component 레벨에서 동작하는 모든 것을 서버 단에서 처리
