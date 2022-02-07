a33cd3441df0f7fe895694c40d41182c2e2ec390
ryuna <anottrx@gmail.com>
2022-02-07 13:12:18 +0900

    fix: .eslintrc.js 수정

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   parser: "@babel/eslint-parser",
   parserOptions: { "requireConfigFile" : "false" }, 
-  babelOptions: { "configFile": "./.babelrc", },
   extends: [
     'plugin:@next/next/recommended',
   ],
