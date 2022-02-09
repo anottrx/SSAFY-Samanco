38bf4e17ae41460d4195646ec28c68e573c13cd1
ryuna <anottrx@gmail.com>
2022-02-09 20:01:06 +0900

    fix: FE-관리자 외의 사람은 회원 상세보기 보지 못함

--- frontend/pages/admin/userinfo.js
+++ frontend/pages/admin/userinfo.js
@@ -1,19 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Router from 'next/router';
 import Layout from '../../components/Layout';
 import UserInfoPage from '../../components/User/UserInfoPage';
 
 export default function AdminUserInfo() {
+  const [isAdmin, setIsAdmin] = useState(false);
+
   useEffect(() => {
-    if (sessionStorage.getItem('nickname') != 'admin') {
+    document.title = '회원 관리 | 싸피사만코';
+
+    if (sessionStorage.getItem('nickname') === 'admin') {
+      setIsAdmin(true);
+    } else {
+      window.history.forward();
       alert('접근 권한이 없습니다');
-      Router.push('/');
+      window.location.replace('/');
+      // Router.push('/');
     }
   }, []);
 
   return (
     <Layout>
-      <UserInfoPage />
+      {isAdmin ? (
+        <>
+          <UserInfoPage />
+        </>
+      ) : (
+        <></>
+      )}
     </Layout>
   );
 }
