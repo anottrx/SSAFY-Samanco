fb0399bd880e00d058b1d839e0656a95a2d145e2
uoayop <uoayop@kakao.com>
2022-02-14 02:51:56 +0900

    feat: FE-개발자 유저 카드, 이미지 버튼 추가

---
+++ frontend/components/Common/ImageButton.js
@@ -0,0 +1,119 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import ButtonBase from '@mui/material/ButtonBase';
+import Typography from '@mui/material/Typography';
+
+const ImageButton = styled(ButtonBase)(({ theme }) => ({
+  position: 'relative',
+  height: 200,
+  [theme.breakpoints.down('sm')]: {
+    width: '100% !important', // Overrides inline-style
+    height: 100,
+  },
+  '&:hover, &.Mui-focusVisible': {
+    zIndex: 1,
+    '& .MuiImageBackdrop-root': {
+      opacity: 0.15,
+    },
+    '& .MuiImageMarked-root': {
+      opacity: 0,
+    },
+    '& .MuiTypography-root': {
+      border: '4px solid currentColor',
+    },
+  },
+  border: '0.5px solid white',
+}));
+
+const ImageSrc = styled('span')({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  backgroundSize: 'cover',
+  backgroundPosition: 'center 40%',
+});
+
+const Image = styled('span')(({ theme }) => ({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color: 'white',
+}));
+
+const ImageBackdrop = styled('span')(({ theme }) => ({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  backgroundColor: 'black',
+  opacity: 0.4,
+  transition: theme.transitions.create('opacity'),
+}));
+
+const ImageMarked = styled('span')(({ theme }) => ({
+  height: 3,
+  width: 18,
+  backgroundColor: 'white',
+  position: 'absolute',
+  bottom: -2,
+  left: 'calc(50% - 9px)',
+  transition: theme.transitions.create('opacity'),
+}));
+
+export default function ButtonBases({ image }) {
+  /*
+        image
+        {
+            url,
+            title,
+            width
+        },
+    */
+
+  return (
+    <ImageButton
+      focusRipple
+      key={image.title}
+      style={{
+        width: image.width,
+      }}
+      onClick={() => {
+        document.location.href = image.link;
+      }}
+    >
+      <ImageSrc style={{ backgroundImage: `url(${image.url})` }} />
+      <ImageBackdrop className="MuiImageBackdrop-root" />
+      <Image>
+        <Typography
+          component="span"
+          variant="subtitle1"
+          color="inherit"
+          sx={{
+            position: 'relative',
+            p: 4,
+            pt: 2,
+            pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
+          }}
+          style={{
+            fontSize: 20,
+            fontWeight: 'bolder',
+            color: 'white',
+            WebkitTextStroke: '0.8px rgb(0 0 0 / 45%)',
+          }}
+        >
+          {image.title}
+          <ImageMarked className="MuiImageMarked-root" />
+        </Typography>
+      </Image>
+    </ImageButton>
+  );
+}
--- frontend/pages/developer/index.js
+++ frontend/pages/developer/index.js
@@ -1,16 +1,15 @@
 import Layout from '../../components/Layout';
 import developerData from '../../data/developerData.json';
+import developerProjectImage from '../../data/developerProjectImage.json';
 import Head from 'next/head';
 import styled from '@emotion/styled';
 import Card from '@mui/material/Card';
 import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import ImageButton from '../../components/Common/ImageButton';
 
 function developer() {
-  const UserWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-  `;
-
   return (
     <Layout>
       <Head>
@@ -25,30 +24,67 @@ function developer() {
       >
         {developerData.map((user) => {
           return (
-            <Grid item xs={12} sm={6} md={4} lg={4} key={user.id}>
+            <Grid item xs={12} sm={6} md={6} lg={6} key={user.id}>
               <UserCard user={user}></UserCard>
             </Grid>
           );
         })}
+        <Grid item xs={12} sm={12} md={12} lg={12}>
+          <ProjectCard></ProjectCard>
+        </Grid>
       </Grid>
     </Layout>
   );
 }
 
-function UserCard({ user }) {
+function ProjectCard() {
   const CusCard = styled(Card)`
     padding: 10px;
     margin-bottom: 20px;
   `;
-  const CusImage = styled.img`
+  const Logo = styled.img`
     width: 100px;
-    height: 100px;
-    border-radius: 100%;
   `;
   return (
     <CusCard>
-      <div>{user.name}</div>
+      {/* <Logo src="/images/main-logo-black.png"></Logo> */}
+      <Box
+        sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          minWidth: 300,
+          width: '100%',
+        }}
+      >
+        {developerProjectImage.map((image) => {
+          return <ImageButton key={image.title} image={image}></ImageButton>;
+        })}
+      </Box>
+    </CusCard>
+  );
+}
+
+function UserCard({ user }) {
+  const CusCard = styled(Card)`
+    padding: 20px;
+    margin-bottom: 10px;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+  `;
+  const CusImage = styled.img`
+    width: 100px;
+    height: 100px;
+    border-radius: 10%;
+    border: 2px solid rgb(0 0 0 / 5%);
+    margin: 0px 20px 0px 0px;
+  `;
+  return (
+    <CusCard>
       <CusImage src={user.image}></CusImage>
+      <Stack>
+        <h3>{user.name}</h3>
+      </Stack>
     </CusCard>
   );
 }
