203b1355316e5ec8755c1636f2cac5ccf46dd005
ryuna <anottrx@gmail.com>
2022-02-13 20:53:43 +0900

    style: 에러페이지 문구 수정

--- frontend/pages/_error.js
+++ frontend/pages/_error.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import Layout from '../components/Layout';
 
 export default class RootError extends Component {
   render() {
-    return 'Error!';
+    return (
+      <Layout>
+        <h2>요청하신 페이지가 존재하지 않습니다.</h2>
+      </Layout>
+    );
   }
 }
