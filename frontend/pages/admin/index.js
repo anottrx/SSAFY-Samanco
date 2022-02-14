a729a071a2c33a651745b1bc00c15e4bc8621a2c
ryuna <anottrx@gmail.com>
2022-02-14 09:47:46 +0900

    style: 회원관리페이지 알림 수정

--- frontend/pages/admin/index.js
+++ frontend/pages/admin/index.js
@@ -2,6 +2,7 @@ import AdminList from '../../components/User/AdminList';
 import React, { useEffect, useState } from 'react';
 import Router from 'next/router';
 import Layout from '../../components/Layout';
+import Swal from 'sweetalert2';
 
 export default function AdminPage() {
   const [isAdmin, setIsAdmin] = useState(false);
@@ -13,8 +14,17 @@ export default function AdminPage() {
       setIsAdmin(true);
     } else {
       window.history.forward();
-      alert('접근 권한이 없습니다');
-      window.location.replace('/');
+      // alert('접근 권한이 없습니다');
+      Swal.fire({
+        title: '접근 권한이 없습니다.',
+        text: '메인페이지로 이동합니다.',
+        icon: 'warning',
+        showConfirmButton: false,
+        timer: 800,
+      }).then(() => {
+        // Router.push('/login');
+        window.location.replace('/');
+      });
       // Router.push('/');
     }
   }, []);
